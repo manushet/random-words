b4b3ec2c2e3b3e0b294e0a1e93f1eb03b7557451
Mariya Nikiforova <manu11shet@gmail.com>
2023-09-23 19:36:38 +0700

    update scripts

--- cgi-bin/words-handler.php
+++ cgi-bin/words-handler.php
@@ -3,17 +3,62 @@
 header("Access-Control-Allow-Origin: *");
 
 $word = $_POST["word"];
+$opt = $_POST["opt"];
 
-if ($word) {
+if ($word && $opt) {
     
-    $file = fopen("words.txt", "a") or die("Unable to open file!");
-    
-    fwrite($file, $word."\n\r");
-    
-    fclose($file);    
+    if ($opt === "add") {
+        $file = fopen("words.txt", "a") or die("Unable to open file!");
+        
+        fwrite($file, $word."\n");
+        
+        fclose($file);          
+    }
+    elseif ($opt === "delete") {
+        
+        $file = fopen("words.txt", "r") or die("Unable to open file!");
+        
+        $words = array();
+        
+        while (!feof($file)) {
+            $el = fgets($file);
+            
+            $el = trim(preg_replace('/\s+/', '', $el));
+            
+            $el = trim(str_replace(array('\r\n', '\n', '\r'), '', $el));
+            
+            if ($el) {
+                $words[] = $el;
+            }
+            
+        }
+        fclose($file);
+        
+        //print_r($words); 
+        
+        $new_array = array();
+        
+        foreach ($words as $el) {
+            if (mb_strtolower($el) !== mb_strtolower($word)) {
+                $new_array[] = $el;
+            }
+        }
+        
+        //print_r($new_array); 
+
+        $file = fopen("words.txt", "w+") or die("Unable to open file!");
+        
+        if (count($new_array) > 0) {
+            foreach ($new_array as $el) {
+                fwrite($file, $el . "\n");
+            }
+        }
+        fclose($file);
+        
+    }
 }
 
-echo "success";
+
 
 
 
